8a5fe0bb43a7dca39f8fda2614225c46aa3789ed
Borsos Brigitte <46690999+borsosbrigitte@users.noreply.github.com>
2020-04-23 21:35:38 +0300

    added more exercises

--- exercises.py
+++ exercises.py
@@ -1,5 +1,45 @@
+from decimal import Decimal
 a=1
 b=2
 
 c = a + b
 print("total is : {}".format(c))
+
+s = "Ana are mere"
+print(s)
+
+print("""acesta este un sir de caractere
+spart pe mai multe randuri
+""")
+
+
+
+
+print("Introdu un nr:")
+
+# x = int(input())
+x = 1
+y = 2
+total = x + y
+
+print("{} + {} este: {}".format(x, y, total))
+
+
+paths = (
+    "Path: \n {} \n {}".format(
+        "c:\\user\\desktop\\file1.py",
+        "c:\\user\\desktop\\file2.py",
+))
+print(paths)
+
+sir = "M"*90
+print(sir)
+
+z = 2**4
+print(z)
+
+d1 = Decimal(10)
+d2 = Decimal(3)
+
+r = d1/d2
+print(r)
