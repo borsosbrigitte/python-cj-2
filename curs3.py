b6668974ac9d688d931e29f1a7f43ca120294b5e
Borsos Brigitte <46690999+borsosbrigitte@users.noreply.github.com>
2020-04-30 19:16:27 +0300

    for and while loops

---
+++ curs3.py
@@ -0,0 +1,24 @@
+print("Curs 3")
+
+# x = 10
+# while x > 1:
+#     print("x este {}".format(x))
+#     x = x - 1
+
+for i in range(1,10,2):
+    print("i este {}".format(i))
+
+for s in "some_string":
+    print(s)
+
+exemplu = "Acesta este un string"
+print("prima litera din sir: {}".format(exemplu[0]))
+
+print("ultima litera din sir este: {}".format(exemplu[-1]))
+print("slicing de siruri de caractere")
+print(exemplu[15:])
+print(exemplu[-6:])
+
+l = len(exemplu)
+print("lungimea sirului este de {}".format(l))
+
